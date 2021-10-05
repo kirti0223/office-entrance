30f7f494946c495f603a177cece1cf7dce611936
kirti0223 <68126346+kirti0223@users.noreply.github.com>
2021-10-05 17:53:06 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,104 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const path = require("path");
+const Data = require("./models/dat");
+const methodOverride = require("method-override");
+
+mongoose
+  .connect("mongodb://localhost:27017/reception-db")
+  .then(() => {
+    console.log("DB CONNECTED");
+  })
+  .catch((er) => {
+    console.log(err);
+  });
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "/views"));
+app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride("_method"));
+app.use(express.static(path.join(__dirname, "public")));
+
+app.get("/", async (req, res) => {
+  const data = await Data.find({});
+  res.render("home", { data });
+});
+app.get("/enter", (req, res) => {
+  res.render("enter");
+});
+app.post("/", async (req, res) => {
+  const { name, email, phone, cinh, cinm } = req.body;
+  sendemail(email, cinh, cinm);
+  await Data.create({ name, email, phone, cinh, cinm });
+  res.redirect("/");
+});
+app.get("/:id", async (req, res) => {
+  const { id } = req.params;
+  const d = await Data.findById(id);
+  res.render("exit", { d });
+});
+app.put("/:id", async (req, res) => {
+  const { id } = req.params;
+  const { couth, coutm } = req.body;
+  const oh = couth;
+  const om = coutm;
+  const d = await Data.findById(id);
+  sendexmail(d.email, oh, om);
+  await Data.findByIdAndUpdate(id, {
+    $set: { status: "Checked Out", couth: oh, coutm, om },
+  });
+  res.redirect("/");
+});
+app.delete("/:id", async (req, res) => {
+  const { id } = req.params;
+  await Data.findByIdAndDelete(id);
+  res.redirect("/");
+});
+
+function sendemail(email, cinh, cinm) {
+  const sgMail = require("@sendgrid/mail");
+  const sendgrid = sgMail.setApiKey(
+    "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"
+  );
+  let m = cinm.toString();
+  let h = cinh.toString();
+  if (cinm <= 9) {
+    m = "0" + cinm.toString();
+  }
+  if (cinh <= 9) {
+    h = "0" + cinh.toString();
+  }
+  const msg = {
+    to: email,
+    from: "kirtisingla58@gmail.com",
+    subject: "Entering building",
+    text: `Hi you entered the building at ${h}:${m}`,
+  };
+  sgMail.send(msg);
+}
+
+function sendexmail(email, couth, coutm) {
+  const sgMail = require("@sendgrid/mail");
+  const sendgrid = sgMail.setApiKey(
+    "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"
+  );
+  let m = coutm.toString();
+  let h = couth.toString();
+  if (coutm <= 9) {
+    m = "0" + coutm.toString();
+  }
+  if (couth <= 9) {
+    h = "0" + couth.toString();
+  }
+  const msg = {
+    to: email,
+    from: "kirtisingla58@gmail.com",
+    subject: "Checking out",
+    text: `Hi you checked out at ${h}:${m}`,
+  };
+  sgMail.send(msg);
+}
+
+app.listen(3000, (req, res) => {
+  console.log("UP AT 3000");
+});
